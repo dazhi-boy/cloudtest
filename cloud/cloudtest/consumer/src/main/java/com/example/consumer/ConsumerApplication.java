cadbd036e4c16aac7f044d35726b2fe3c9d1d646
zhiyong.guo <zhiyong.guo@bioknow.net>
2020-08-20 18:10:45 +0800

    spring cloud test program

--- cloud/cloudtest/consumer/src/main/java/com/example/consumer/ConsumerApplication.java
+++ cloud/cloudtest/consumer/src/main/java/com/example/consumer/ConsumerApplication.java
@@ -1,5 +1,6 @@
 package com.example.consumer;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -21,6 +22,7 @@ public class ConsumerApplication {
     @FeignClient("name")
     static interface NameService {
         @RequestMapping("/")
+        @SentinelResource("/")
         public String getName();
     }
 }
